b2d17bfa8c39ca3659b6a90a1fb4b51504b24f55
DevanshuV25 <97956986+DevanshuV25@users.noreply.github.com>
2023-04-23 22:05:14 +0530

    initial commit

---
+++ app/src/main/java/com/devanshu/instagramproject/MainActivity.java
@@ -0,0 +1,53 @@
+package com.devanshu.instagramproject;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        view = findViewById(R.id.bottomNavigationView2);
+        replacefragement(new Fragment_one());
+        view.setOnItemSelectedListener(item ->  {
+
+                switch (item.getItemId()){
+
+                    case R.id.home :
+                        replacefragement(new Fragment_one());
+                        break;
+                    case R.id.search:
+                        replacefragement(new Fragment_three());
+                        break;
+                    case R.id.profile:
+                        replacefragement(new Fragment_two());
+                        break;
+
+                }
+
+                return true;
+        });
+    }
+
+    private void replacefragement(Fragment fragment)
+    {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction() ;
+        fragmentTransaction.replace(R.id.framelayout,fragment);
+        fragmentTransaction.commit();
+    }
+}
